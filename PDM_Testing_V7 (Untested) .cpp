42233e8f6f86111d38f8ee2f5083fdac8f13917f
FranciscoFadigasMendes <79844404+FranciscoFadigasMendes@users.noreply.github.com>
2023-05-11 10:59:28 +0100

    Update PDM_Testing_V7 (Untested) .cpp

--- PDM_Testing_V7 (Untested) .cpp
+++ PDM_Testing_V7 (Untested) .cpp
@@ -9,25 +9,25 @@ Author: Francisco Fadigas Mendes
 #define ADCPIN_6 A6     // Potentiometer -> ADC1_CH6
 #define ADCPIN_7 A7     // Button -> ADC1_CH7
 #define ADCPIN_18 A18   // Source Voltage -> ADC2_CH8
-#define ADCPIN_19 A19     //Cooling Board Current Sense -> ADC2_CH9
+#define ADCPIN_19 A19   //Cooling Board Current Sense -> ADC2_CH9
 
 //Functions
 void can_bus();
-void inputs_status();
-void outputs_status();
+//void input_status();
+//void output_status();
 void turn_on_sequence();
 void cooling_board(); 
 void brakelight();
 void terminal();
 
 //CAN Bus Setup
-TWAI_Interface CAN1(1000,4,5); // BaudRate/1000 , 4 TX , 5 RX
+TWAI_Interface CAN1(1000,21,22); // BaudRate/1000 , TX , RX
 
 //Global Variables
 int adc6Value,adc7Value,adc18Value,adc19Value; //ADC RAW VALUES
-unsigned long volts_print_time = 0, can_time = 0, cb_time=0; // VARIABLES FOR MILLIS FUCNTION
+unsigned long volts_print_time = 0, can_time = 0, cb_time=0, terminal_cycles_counter = 0;; // VARIABLES THAT MEASURE TIME
 
-int increment = 0,FAN,terminal_cycles_counter=0;
+int increment = 0,FAN;
 float PotVoltValue,InputVoltValue,buttonValue,cooling_power,cooling_p,InputVoltValue_ADC; 
 
 
@@ -38,8 +38,13 @@ void setup() {
 
   Serial.begin(115200);
 
+  pinMode(A6, INPUT);
+  pinMode(A7, INPUT);
+  pinMode(A18, INPUT);
+  pinMode(A19, INPUT);
+
   pinMode(23, OUTPUT); // Cooling Board 
-  pinMode(22, OUTPUT); // DataLogger
+  pinMode(18, OUTPUT); // DataLogger
   pinMode(19, OUTPUT); // Brake Light
 
   }
@@ -48,19 +53,25 @@ void setup() {
 
 void loop() {
 
-  terminal_cycles_counter++;
-
-  Serial.println("----------<= terminal_cycles_counter =>---------");
 
     if( (millis() - can_time) > 333){  // 3 times a second
-      can_time = millis();
-      turn_on_sequence();
-      cooling_board(); 
-      can_bus();
-      terminal();
+
+    terminal_cycles_counter++;
+
+      Serial.print("----------<= ");
+      Serial.print(terminal_cycles_counter);
+      Serial.println(" =>---------");
+
+        can_time = millis();
+        turn_on_sequence();
+        cooling_board(); 
+        can_bus();
+        terminal();
+
+      Serial.println("--------------------------------");
+      Serial.println("");
     }
 
-  Serial.println("--------------------------------");
 }
 
 //----------------------------------------------------------------
@@ -68,11 +79,11 @@ void loop() {
 
 void can_bus(){
 
-  int InputVoltValue_Offset = InputVoltValue * 100;
-  Serial.println(InputVoltValue_Offset);
+  //input_status();
+  //output_status();
 
-  inputs_status();
-  outputs_status();
+  //Source Voltage
+  int InputVoltValue_Offset = InputVoltValue * 100;
 
   //---------------------CAN ID40 MESSAGE BEGIN---------------------
   CAN1.TXpacketBegin(0x40,0);    
@@ -144,17 +155,19 @@ void can_bus(){
 }
 
 //----------------------------------------------------------------
-void outputs_status(){
+/*
+void intput_status(){
 
 
 
 }
 
-void outputs_status(){
+void output_status(){
 
 
 
 }
+*/
 //----------------------------------------------------------------
 
 void turn_on_sequence(){
@@ -171,9 +184,9 @@ void turn_on_sequence(){
 
     //DataLogger Power On Values
     if(PotVoltValue >= 1)     
-      digitalWrite(22, HIGH); 
+      digitalWrite(18, HIGH); 
     if(PotVoltValue < 1)
-      digitalWrite(22, LOW); 
+      digitalWrite(18, LOW); 
 
     //Brake Light Power On Values
     if(PotVoltValue >= 1.5)     
@@ -197,9 +210,11 @@ void cooling_board(){
     cooling_p = ((adc19Value * 3.3) / 4095);
     cooling_power = ( (cooling_p / (0.002*50)) + 0.08 );
 
+    //Increments Every the Button is Pressed
     if(buttonValue>1.5)
       increment++;
 
+    //Even Number-FAN = 20 ; Odd Number-FAN = 30
     if(increment % 2 == 0)
       FAN=20;   
     else
@@ -211,20 +226,23 @@ void cooling_board(){
 
 void terminal(){
 
+    //Reads Source Voltage
     adc18Value = analogRead(ADCPIN_18);
     InputVoltValue_ADC = ((adc18Value * 3.3) / 4095);
-     
     InputVoltValue = InputVoltValue_ADC * 11.7;
 
+    //Prints Source Voltage
     Serial.print("Source Voltage = ");
     Serial.print(InputVoltValue, 3);
     Serial.println(" V ");
 
+    //Prints POT Voltage
     Serial.print("POT = ");
     Serial.print(PotVoltValue, 3);
     Serial.println(" V ");
 
-    Serial.print("Increment =  ");
+    //Prints Button Presses
+    Serial.print("Button Presses =  ");
     Serial.println(increment);
 
 }
